9770af521707d2148f33f012d785195521d54777
JinhaeCoder <justezy0210@gmail.com>
2021-05-27 14:33:54 +0900

    fix settings

--- proj/settings.py
+++ proj/settings.py
@@ -24,12 +24,26 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env(
+  os.path.join(BASE_DIR, '.env')
+)
+
+# False if not in os.environ
+DEBUG = env('DEBUG')
+
+# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%np$1f7!2ah-=b*&lmj@-ng!p+4ez@$i-+ekijp)bcrp6e_^ui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
